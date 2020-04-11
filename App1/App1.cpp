eba5b7d0cfd75b12a01b74ef3969f6f4bda2ec27
artemtmgm <artemtmgm@gmail.com>
2020-04-11 19:16:19 +0300

    Добавьте файлы проекта.

---
+++ App1/App1.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream> 
+#include <time.h> 
+
+using namespace std;
+
+void view(const int*, int);
+void MegNum(int*, int);
+void SelNum(int*, int);
+void createVect(int*&, int);
+
+void Invert(int*, int);
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    int n;
+    int* pA, * pB, * pC;
+    cout << "Задайте кiлькiсть елементiв: ";
+    cin >> n;   
+    pA = new int[n];     
+    createVect(pA, n);      
+    view(pA, n);
+    MegNum(pA, n);
+    SelNum(pA, n);
+    cout << "Iнвертований ";
+    Invert(pA, n);
+    view(pA, n);
+    cout << endl;
+
+    cout << "Задайте кiлькiсть елементiв: ";
+    cin >> n;   
+    pB = new int[n];
+    createVect(pB, n);
+    view(pB, n);
+    MegNum(pB, n);
+    SelNum(pB, n);
+    cout << "Iнвертований ";
+    Invert(pB, n);
+    view(pB, n);
+    cout << endl;
+
+    cout << "Задайте кiлькiсть елементiв: ";
+    cin >> n;   
+    pC = new int[n];
+    createVect(pC, n);
+    view(pC, n);
+    MegNum(pC, n);
+    SelNum(pC, n);
+    cout << "Iнвертований ";
+    Invert(pC, n);
+    view(pC, n);
+
+    delete[]pA;
+    delete []pB;       
+    delete[]pC;
+    return 0;
+} 
+void view(const int* X, int size) {
+    cout << "Масив: "<< '\t';
+    for (int i = 0; i < size; i++) {
+        cout << X[i] << '\t';
+    }
+    cout << endl;
+}
+
+void MegNum(int* X, int size) {
+    cout << "Масив елементiв з протилежним знаком: " << '\t';
+    for (int i = 0; i < size; i++) {
+            cout << X[i]*-1 << '\t';
+    }
+    cout << endl;
+}
+
+void SelNum(int* X, int size) {
+    cout << "Масив непарних елементiв: " << '\t';
+    for (int i = 0; i < size; i++) {
+      if (X[i] % 2 == 0) 
+        cout << "0" << '\t';
+      else 
+        cout << X[i] << '\t';
+    }
+    cout << endl;
+}
+
+void createVect(int*& X, int size) {
+    srand((time(NULL)));
+    for (int i = 0; i < size; i++)
+        X[i] = rand() % 21 - 10;
+}
+
+
+void Invert(int* X, int size) {
+    int temp;
+    for (int i = 0; i < size / 2; i++)
+    {
+        temp = X[i];
+        X[i] = X[size - 1 - i];
+        X[size - 1 - i] = temp;
+    }
+}
---
+++ App2/App2.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream> 
+#include <time.h> 
+
+using namespace std;
+
+void view(const int*);
+bool checkDate(int*, int);
+void createVect(int*&, int, int);
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    int* pA, * pB, * pC;
+    bool day, mounth, year;
+    pA = new int[10];
+    createVect(pA, 1, 30);
+    cout << "Days: " << '\t';
+    view(pA);
+   
+    pB = new int[10];
+    createVect(pB, 1, 12);
+    cout << "Months: " << '\t';
+    view(pB);
+ 
+    pC = new int[10];
+    createVect(pC, 1999, 2003);
+    cout << "Years: " << '\t';
+    view(pC);
+
+    day = checkDate(pA,16);
+    mounth = checkDate(pB, 1);
+    year = checkDate(pC, 2001);
+    if (day == true && mounth == true && year == true)
+        cout << "You are lucky!";
+    else
+        cout << "Try again!";
+   
+    delete[]pA;
+    delete[]pB;
+    delete[]pC;
+    return 0;
+}
+void view(const int* X) {
+    for (int i = 0; i < 10; i++) {
+        cout << X[i] << '\t';
+    }
+    cout << endl;
+}
+
+void createVect(int*& X, int a, int b) {
+    srand((time(NULL)));
+    for (int i = 0; i < 10; i++)
+        X[i] = a + rand() % ((b+1)-a);
+}
+
+bool checkDate(int* X, int a) {
+    for (int i = 0; i < 10; i++) {
+        if (X[i] == a) {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
